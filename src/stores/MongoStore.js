cfdcaf6c52232222e91430f9634b53a8d4ca6553
AEtheve <30652308+AEtheve@users.noreply.github.com>
2022-02-27 20:08:07 +0100

    update dependencies

--- src/stores/MongoStore.js
+++ src/stores/MongoStore.js
@@ -1,54 +1,54 @@
-import { MongoClient } from "https://deno.land/x/mongo@v0.28.0/mod.ts";
+import { MongoClient } from "https://deno.land/x/mongo@vv0.29.2/mod.ts";
 
 export default class MongoStore {
-  constructor(options) {
-    this.uri = options.uri || "mongodb://127.0.0.1:27017";
-    this.collection = options.collection || "sessions";
-    this.connOptions = options.connectionOptions ||
-      ({ useNewUrlParser: true, useUnifiedTopology: true });
-    this.databaseName = options.databaseName || null;
-    this.db = null;
-  }
-  async init() {
-    this.client = new MongoClient();
+    constructor(options) {
+        this.uri = options.uri || "mongodb://127.0.0.1:27017";
+        this.collection = options.collection || "sessions";
+        this.connOptions = options.connectionOptions ||
+            ({ useNewUrlParser: true, useUnifiedTopology: true });
+        this.databaseName = options.databaseName || null;
+        this.db = null;
+    }
+    async init() {
+        this.client = new MongoClient();
 
-    await this.client.connect(this.uri, this.connOptions);
+        await this.client.connect(this.uri, this.connOptions);
 
-    this.db = this.databaseName == null
-      ? this.client.database()
-      : this.client.database(this.databaseName);
-  }
+        this.db = this.databaseName == null ?
+            this.client.database() :
+            this.client.database(this.databaseName);
+    }
 
-  async sessionExists(sessionId) {
-    const session = await this.db.collection(this.collection).findOne({
-      sessionid: sessionId,
-    });
-    return session ? true : false;
-  }
+    async sessionExists(sessionId) {
+        const session = await this.db.collection(this.collection).findOne({
+            sessionid: sessionId,
+        });
+        return session ? true : false;
+    }
 
-  async getSessionById(sessionId) {
-    const value = (await this.db.collection(this.collection).findOne({
-      sessionid: sessionId,
-    })).data;
-    return value;
-  }
+    async getSessionById(sessionId) {
+        const value = (await this.db.collection(this.collection).findOne({
+            sessionid: sessionId,
+        })).data;
+        return value;
+    }
 
-  async createSession(sessionId) {
-    await this.db.collection(this.collection).insert({
-      sessionid: sessionId,
-      data: { "_flash": {} },
-    });
-  }
+    async createSession(sessionId) {
+        await this.db.collection(this.collection).insert({
+            sessionid: sessionId,
+            data: { "_flash": {} },
+        });
+    }
 
-  async persistSessionData(sessionId, sessionData) {
-    await this.db.collection(this.collection).updateOne({
-      sessionid: sessionId,
-    }, { $set: { data: sessionData } });
-  }
+    async persistSessionData(sessionId, sessionData) {
+        await this.db.collection(this.collection).updateOne({
+            sessionid: sessionId,
+        }, { $set: { data: sessionData } });
+    }
 
-  async destroySession(sessionId) {
-    await this.db.collection(this.collection).deleteOne({
-      sessionid: sessionId,
-    });
-  }
+    async destroySession(sessionId) {
+        await this.db.collection(this.collection).deleteOne({
+            sessionid: sessionId,
+        });
+    }
 }
